8e46e1c814bff6f1b90e04d08d33c78c4c18517e
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-08-23 01:13:33 +0300

    created review model

---
+++ src/models/review.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+    {
+        reviewerId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        feedback: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number,
+            required: true,
+            min: 1,
+            max: 5,
+        }
+    },
+    { timestamps: true }
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+
+export default Review;
