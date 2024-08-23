485923384a1777393a783b835d3a95b562c4b667
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-08-23 16:48:23 +0300

    created editUser in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -30,3 +30,31 @@ export const getUserByUsername = async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 }
+
+export const editUser = async (req, res) => {
+
+    try {
+        const id = req.user._id;
+        const { displayName, email, username, gender, age, picture, learn, teach } = req.body;
+
+        const user = await User.findById(id);
+
+        user.displayName = displayName ?? user.displayName;
+        user.email = email ?? user.email;
+        user.username = username ?? user.username;
+        user.gender = gender ?? user.gender;
+        user.age = age ?? user.age;
+        user.picture = picture ?? user.picture;
+        user.learn = learn ?? user.learn;
+        user.teach = teach ?? user.teach;
+
+        console.log(user);
+
+        await user.save();
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.error("Error in getUsersBySearch: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
