a71bac3005562572f2ee18f54922863856edeab8
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-08-23 17:11:00 +0300

    added different routes to server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import chatRoutes from "./routes/chat.routes.js";
+import reviewRoutes from "./routes/review.routes.js";
+import categoryRoutes from "./routes/category.routes.js"
+
 import connectToMongoDB from "./database/connect.js";
 import { app, server } from "./socket/socket.js";
 
@@ -11,6 +18,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/chats", chatRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/categories", categoryRoutes);
 
 server.listen(PORT, () => {
     connectToMongoDB();
