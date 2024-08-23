f36ce31887eb41859692ddf5c30610252fd304f3
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-08-23 03:07:48 +0300

    created auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,109 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res) => {
+    try {
+        const { displayName, username, email, password, confirmPassword, gender } = req.body;
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Passwords don't match" });
+        }
+
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" });
+        }
+
+        user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ error: "Email already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(12);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const profilePic = (gender === "male"
+            ? `https://avatar.iran.liara.run/public/boy?username=${username}`
+            : `https://avatar.iran.liara.run/public/girl?username=${username}`
+        );
+
+        const newUser = new User({
+            displayName,
+            username,
+            email,
+            password: hashedPassword,
+            gender,
+            profilePic,
+        });
+
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                displayName: newUser.displayName,
+                username: newUser.username,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+        } else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+
+        const user = email ? await User.findOne({ email }) : await User.findOne({ username });
+
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({ error: `Invalid ${email ? 'email' : 'username'} or password` });
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            displayName: user.displayName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export const logout = (req, res) => {
+    try {
+        res.cookie("token", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+const generateTokenAndSetCookie = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "15d",
+    });
+
+    res.cookie("token", token, {
+        maxAge: 15 * 24 * 60 * 60 * 1000,
+        httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+        sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+        secure: process.env.NODE_ENV !== "development",
+    });
+};
