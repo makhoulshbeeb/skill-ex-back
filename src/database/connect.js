63907a11e6b71e57123b375069d0b09535f289a4
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-08-23 03:24:47 +0300

    created MongoDB connection

---
+++ src/database/connect.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default async function connectToMongoDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URI);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log("Error connecting to MongoDB", error.message);
+    }
+}
--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import connectToMongoDB from "./database/connect";
+
+dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
