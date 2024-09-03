57898961e35b901e434aeeb8189718d104b85040
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-03 10:51:18 +0300

    updated user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import userAuth from "../middleware/userAuth.js";
-import { deleteUser, editUser, getUserByUsername, getUsersByMatch, getUsersBySearch } from "../controllers/user.controller.js";
+import { deleteUser, editUser, getUserByToken, getUserByUsername, getUsersByMatch, getUsersBySearch } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/:username", getUserByUsername);
+router.get("/me", userAuth, getUserByToken)
 router.get("/search/:search", userAuth, getUsersBySearch);
 router.get("/match/", userAuth, getUsersByMatch);
 router.patch("/", userAuth, editUser);
