663df8f8ed3836be24bc9f85c175b2085b3b674c
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-09 00:40:46 +0300

    added get get by id to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import userAuth from "../middleware/userAuth.js";
-import { deleteUser, editUser, endorseUser, getUserByToken, getUserByUsername, getUsersByMatch, getUsersBySearch } from "../controllers/user.controller.js";
+import { deleteUser, editUser, endorseUser, getUserById, getUserByToken, getUserByUsername, getUsersByMatch, getUsersBySearch } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/user/:username", getUserByUsername);
+router.get("/id/:id", getUserById);
 router.get("/me", userAuth, getUserByToken)
 router.get("/search/:search", userAuth, getUsersBySearch);
 router.get("/match/", userAuth, getUsersByMatch);
