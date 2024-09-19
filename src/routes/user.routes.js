3a5fa0f58fecd094592e012a0e2261e0594a911e
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-19 21:27:53 +0300

    updated user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,20 +1,23 @@
 import express from "express";
 import userAuth from "../middleware/userAuth.js";
-import { deleteUser, editUser, endorseUser, getUserById, getUserByToken, getUserByUsername, getUsersByMatch, getUsersBySearch } from "../controllers/user.controller.js";
+import { deleteUser, editUser, endorseUser, getUserById, getUserByToken, getUserByUsername, getUsersByMatch, getUsersBySearch, updateRole } from "../controllers/user.controller.js";
 import adminAuth from "../middleware/adminAuth.js";
 
 const router = express.Router();
 
 router.get("/user/:username", getUserByUsername);
 router.get("/id/:id", getUserById);
+
 router.get("/me", userAuth, getUserByToken);
-router.get("/admin", adminAuth, getUserByToken);
 router.get("/search/:search", userAuth, getUsersBySearch);
 router.get("/search/", userAuth, getUsersBySearch);
 router.get("/match/", userAuth, getUsersByMatch);
 router.patch("/endorse/:id", userAuth, endorseUser);
 router.patch("/", userAuth, editUser);
 router.delete("/", userAuth, deleteUser);
+
+router.get("/admin", adminAuth, getUserByToken);
+router.patch("/role/:id", adminAuth, updateRole);
 router.delete("/:id", adminAuth, deleteUser);
 
 export default router;
