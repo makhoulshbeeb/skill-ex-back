140f554de2130f9e3cddb57b63fd9e68c4d1a26d
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-19 20:02:15 +0300

    added new category routes

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { getCategories } from "../controllers/category.controller.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const router = express.Router();
 
 router.get('/', getCategories);
+router.post('/', adminAuth, getCategories);
+router.patch('/:id', adminAuth, getCategories);
+router.delete('/:id', adminAuth, getCategories);
 
 export default router;
