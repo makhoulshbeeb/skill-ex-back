dc542027b331b54259e718e9b8c77e10cbb8178a
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-19 20:06:35 +0300

    added delete review admin route

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { deleteReview, getReviews, addReview } from "../controllers/review.controller.js";
+import { deleteReview, getReviews, addReview, deleteReviewAdmin } from "../controllers/review.controller.js";
 import userAuth from "../middleware/userAuth.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const router = express.Router();
 
 router.get("/:id", getReviews);
 router.post("/:id", userAuth, addReview);
 router.delete("/:id", userAuth, deleteReview);
+router.delete("/admin/:id", adminAuth, deleteReviewAdmin);
 
 export default router;
