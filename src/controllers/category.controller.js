9d77795d4b2afe8e4b9e274583e4486f66d15ea3
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-19 19:58:51 +0300

    added new category functions to controller

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -6,7 +6,50 @@ export const getCategories = async (req, res) => {
 
         res.status(200).json(categories);
     } catch (error) {
-        console.log("Error in getChats controller: ", error.message);
+        console.log("Error in getCategories controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+export const addCategory = async (req, res) => {
+    try {
+        const { name, picture } = req.body;
+        const category = await Category.create(
+            name,
+            picture
+        );
+        await category.save();
+        res.status(200).json(category);
+    } catch (error) {
+        console.log("Error in addCategory controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+export const editCategory = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, picture } = req.body;
+        const category = await Category.findById(id);
+
+        category.name = name ?? category.name;
+        category.picture = picture ?? category.picture;
+
+        await category.save();
+        res.status(200).json(category);
+    } catch (error) {
+        console.log("Error in editCategory controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+export const deleteCategory = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Category.findByIdAndDelete(id);
+
+        res.status(204).json({ response: "Category deleted successfully" });
+    } catch (error) {
+        console.log("Error in getCategories controller: ", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 }
