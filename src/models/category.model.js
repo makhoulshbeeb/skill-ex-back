7c11fea14a7f6f72438dc6f5130aebae33c0cf39
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-19 22:32:14 +0300

    updated categories model

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -6,7 +6,6 @@ const categorySchema = new mongoose.Schema(
             type: String,
             required: true,
             unique: true,
-            enum: ["Web Development", "Data Science", "Cybersecurity", "Artificial Intelligence", "Mobile App Development", "Arts & Crafts", "Business & Entrepreneurship", "Personal Development", "Health & Wellness", "Languages", "Music & Performing Arts", "Cooking & Culinary Arts", "Writing & Literature", "Science & Engineering", "Marketing & Sales", "Design & Creative", "Finance & Investing", "Photography & Videography", "Public Speaking & Communication", "Leadership & Management", "Education", "DIY & Home Improvement", "Fitness & Sports", "Mindfulness & Meditation"],
         },
         picture: {
             type: String,
