d7e30b294f9951be61a4d32a352365bf927b6015
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-09-19 20:50:46 +0300

    added get user by token for admins

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import userAuth from "../middleware/userAuth.js";
 import { deleteUser, editUser, endorseUser, getUserById, getUserByToken, getUserByUsername, getUsersByMatch, getUsersBySearch } from "../controllers/user.controller.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const router = express.Router();
 
 router.get("/user/:username", getUserByUsername);
 router.get("/id/:id", getUserById);
-router.get("/me", userAuth, getUserByToken)
+router.get("/me", userAuth, getUserByToken);
+router.get("/admin", adminAuth, getUserByToken);
 router.get("/search/:search", userAuth, getUsersBySearch);
 router.get("/search/", userAuth, getUsersBySearch);
 router.get("/match/", userAuth, getUsersByMatch);
